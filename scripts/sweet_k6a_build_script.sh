4a6c1014e614041ea7af9f4a226c7ac4e07e2bc0
Hakan <hknhzl@gmail.com>
2024-05-26 00:10:53 +0300

    Update sweet_k6a_build_script.sh

--- scripts/sweet_k6a_build_script.sh
+++ scripts/sweet_k6a_build_script.sh
@@ -35,6 +35,7 @@ DEVICE="Redmi Note 12 Pro"
 export DEVICE
 CODENAME="sweet_k6a"
 export CODENAME
+DEFCONFIG="sweet.config"
 DEFCONFIG="sweet_user_defconfig"
 export DEFCONFIG
 COMMIT_HASH=$(git rev-parse --short HEAD)
